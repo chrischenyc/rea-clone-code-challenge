af60b07f643769ea4d5f947b422a287a08ca5314
Chris Chen <chrischen79@gmail.com>
2020-05-14 19:47:24 +1000

    web: add router

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import Home from './pages/Home';
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
